1ce4ff1dcea2cef578a00af97db43d8cec138d05
Aritracodmw3 <36726699+Aritracodmw3@users.noreply.github.com>
2022-04-23 10:03:59 +0530

    Update script.upp.sh

--- script.upp.sh
+++ script.upp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for (( c=0; c<$(gpu-detect listjson | jq 'length'); c++ ))\
-do i=$c;\
-if [ "$(gpu-detect listjson | jq '.['$i'] | .name')" == '"Radeon RX 6800"' ];\
-then\
-echo "GPU $c is RX 6800";\
-upp -p /sys/class/drm/card$c/device/pp_table setsmc_pptable/FreqTableFclk/0=1550 --write;\
-else echo "GPU $c is not supported";\
+for (( c=0; c<$(gpu-detect listjson | jq 'length'); c++ ))
+do i=$c;
+if [ "$(gpu-detect listjson | jq '.['$i'] | .name')" == '"Radeon RX 6800"' ];
+then
+echo "GPU $c is RX 6800";
+upp -p /sys/class/drm/card$c/device/pp_table setsmc_pptable/FreqTableFclk/0=1550 --write;
+else echo "GPU $c is not supported";
 done
-fi\
+fi
